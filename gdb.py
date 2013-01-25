14510de3f8b6f0ffd6d5cfee31efd841cb629cff
dazzyd <dazzyd2@gmail.com>
2013-01-25 16:22:02 +0800

    main.py:OauthS

--- gdb.py
+++ gdb.py
@@ -6,20 +6,21 @@ from google.appengine.ext import db
 import logging
 
 class UserModel(db.Model):
+	user_id		= db.IntegerProperty(required=True)
 	token		= db.StringProperty(required=True)  # Access token
 	secret		= db.StringProperty(required=True)  # Access token secret
 	auth_time	= db.DateTimeProperty(auto_now_add=True)
-	username	= db.StringProperty(required=True)
+	screen_name	= db.StringProperty()
 	tweet_sum	= db.IntegerProperty()  # user's tweet count
 	tweet_last	= db.IntegerProperty()  # user's last tweet timestamp
 
 # Save new user
-def save_user(self, new_token, new_secret):
-	res = UserModel.all().filter('token = ', new_token)
+def save_user(self, user_id, new_token, new_secret):
+	res = UserModel.all().filter('user_id = ', user_id)
 	if res.count() > 0:
 	db.delete(res)
 	
-	user = UserModel( token = new_token, secret = new_secret)
+	user = UserModel(user_id=user_id, token=new_token, secret=new_secret)
 	user.put()
 
 # Get all users
@@ -27,11 +28,11 @@ def get_all_users(self):
 	return UserModel.all()
 
 # Delete user
-def delete_user(self, del_token, del_secret):
+def delete_user(self, user_id, del_token, del_secret):
 	res = UserModel.all().filter('token = ', del_token).filter('secret = ', del_secret)
 	
 	if res.count > 0:
 		db.delete(res)
 	else:
 		logging.error("Gdb.delete() mismatch.")
-		logging.error("token: %s\nsecret:%s" % (token, secret))
+		logging.error("user_id:%d\ntoken: %s\nsecret:%s" % (user_id, del_token, del_token))
--- main.py
+++ main.py
@@ -3,6 +3,54 @@
 # @author:	Dazzy Ding (dazzyd, @ks_magi)
 
 import webapp2
+import logging
+
+import oauth, gdb
+
+CONSUMER_KEY = 'CjUSfZ1IDHrBe9dLu5Viyw'
+CONSUMER_SECRET = '3iUPswMl97gxxirCK5rVN3MvNqM5AcB1dTnxlmdMyQ'
+
+class OauthS(webapp2.RequestHandler):
+	def get(self, mode=''):
+		client = oauth.TwitterClient( CONSUMER_KEY, CONSUMER_SECRET, "%s/oauth/verify" % self.request.host_url )
+		
+		if mode=='auth':
+			if self.request.get('key','QwQ') == AUTH_KEY:
+				# step C Consumer Direct User to Service Provider
+				try:
+					url = client.get_authorization_url()
+					self.redirect(url)
+				except Exception,error_message:
+					logging.error("Auth Error:")
+					logging.error(error_message)
+					self.response.out.write( error_message )
+				
+			else:
+				# Redirect unauth user to main page
+				self.redirect(self.request.host_url)
+			
+		if mode=='verity':
+			# step D Service Provider Directs User to Consumer
+			auth_token = self.request.get("oauth_token")
+			auth_verifier = self.request.get("oauth_verifier")
+			
+			# step E Consumer Request Access Token 
+			# step F Service Provider Grants Access Token
+			try:
+				user_id, access_token, access_secret = client.get_access_token(auth_token, auth_verifier)
+				gdb.save_user(user_id, access_token, access_secret)
+				self.redirect( "%s/oauth/succ?user_id=%d" % (self.request.host_url, user_id) )
+				
+			except Exception,error_message:
+				logging.error("/auth/verity Error:")
+				logging.error("user_id: %d\nauth_token: %s\nauth_verifier: %s" % (auth_token, auth_verifier))
+				logging.error("user_id: %d\ntoken: %s\nsecret: %s" % (user_id, access_token, access_secret))
+				logging.error( error_message )
+				self.response.out.write( error_message )
+			
+		if mode=='succ':
+			self.response.out.write( 'You have authorize this app. <br/>$username: %d' % self.request.get("user_id") )
+		
 
 class DefaultS(webapp2.RequestHandler):
 	def get(self):
@@ -18,9 +66,8 @@ class DefaultS(webapp2.RequestHandler):
 </html>'''
 		self.response.out.write( message.replace('#version#', VERSION).replace('#MIN#', str(TWEET_MIN)) )
 
-app = webapp2.WSGIApplication( [
+app = webapp2.WSGIApplication([
 		(r'/oauth/(.*)',	OauthS	),
 		(r'/pre/(.*)',		PreS	),
-		(r'/poster/(.*)',	PosterS	),
 		(r'/(.*)',			DefaultS)
-		], debug=True)
+		])
