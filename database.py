f38644f1b8ffbe85d0be3e5dd431078544a06f9c
dazzyd <dazzyd2@gmail.com>
2013-01-25 15:37:57 +0800

    ...

--- database.py
+++ database.py
@@ -10,29 +10,28 @@ class UserModel(db.Model):
 	secret		= db.StringProperty(required=True)  # Access token secret
 	auth_time	= db.DateTimeProperty(auto_now_add=True)
 	username	= db.StringProperty(required=True)
-	user_sum	= db.IntegerProperty()  # user's tweet count
-	user_last	= db.IntegerProperty()  # user's last tweet timestamp
+	tweet_sum	= db.IntegerProperty()  # user's tweet count
+	tweet_last	= db.IntegerProperty()  # user's last tweet timestamp
 
 # Save new user
-def db_save_new(new_token, new_secret):
+def save_user(self, new_token, new_secret):
 	res = UserModel.all().filter('token = ', new_token)
 	if res.count() > 0:
-		db.delete(res)
+	db.delete(res)
 	
-	user = UserModel( token = new_token,
-					  secret = new_secret)
+	user = UserModel( token = new_token, secret = new_secret)
 	user.put()
 
 # Get all users
-def db_get_all():
+def get_all_users(self):
 	return UserModel.all()
 
 # Delete user
-def db_delete(del_token, del_secret):
+def delete_user(self, del_token, del_secret):
 	res = UserModel.all().filter('token = ', del_token).filter('secret = ', del_secret)
 	
 	if res.count > 0:
 		db.delete(res)
 	else:
-		logging.error("delete_model mismatch. token: " + token)
-        logging.error("delete_model mismatch. secret: " + secret)
+		logging.error("Gdb.delete() mismatch.")
+		logging.error("token: %s\nsecret:%s" % (token, secret))
