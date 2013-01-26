131e9db201d31aefb72632d221cc3f8d8f09cde8
dazzyd <dazzyd2@gmail.com>
2013-01-26 20:37:51 +0800

    main.py:PostS

--- gdb.py
+++ gdb.py
@@ -3,36 +3,73 @@
 # @author:	Dazzy Ding (dazzyd, @ks_magi)
 
 from google.appengine.ext import db
+import json
 import logging
 
-class UserModel(db.Model):
+class User(db.Model):
 	user_id		= db.IntegerProperty(required=True)
 	token		= db.StringProperty(required=True)	# Access token
 	secret		= db.StringProperty(required=True)	# Access token secret
 	auth_time	= db.DateTimeProperty(auto_now_add=True)
-	screen_name	= db.StringProperty()
-	tweet_sum	= db.IntegerProperty()	# user's tweet count
-	tweet_last	= db.IntegerProperty()	# user's last tweet timestamp
+	tweet_last	= db.IntegerProperty()
+	tweet_count	= db.StringProperty()
+
+
+# Get all users
+def all_users():
+	return User.all()
 
 # Save new user
 def save_user(user_id, new_token, new_secret):
-	res = UserModel.all().filter('user_id = ', user_id)
+	res = User.all().filter('user_id = ', user_id)
 	if res.count() > 0:
 		db.delete(res)
 	
-	user = UserModel(user_id=user_id, token=new_token, secret=new_secret)
+	user = User(user_id=user_id, token=new_token, secret=new_secret, tweet_last=0, tweet_count='{"rt": 0, "re": 0, "sum": 0, "rts": 0}')
 	user.put()
-
-# Get all users
-def get_all_users():
-	return UserModel.all()
 
 # Delete user
 def delete_user(user_id, del_token, del_secret):
-	res = UserModel.all().filter('token = ', del_token).filter('secret = ', del_secret)
+	res = User.all().filter('token = ', del_token).filter('secret = ', del_secret)
 	
 	if res.count > 0:
 		db.delete(res)
 	else:
-		logging.error("Gdb.delete() mismatch.")
-		logging.error("user_id:%d\ntoken: %s\nsecret:%s" % (user_id, del_token, del_token))
+		logging.error("gdb.delete() Mismatch.")
+		logging.error("user_id: %d\ntoken: %s\nsecret: %s" % (user_id, del_token, del_token))
+
+
+# Save user's count
+def save_count(user_id, sum, re, rt, rts, last):
+	res = User.all().filter("user_id = ", user_id)
+	if res.count()==1:
+		json_str = json.dumps( {"sum":sum, "re":re, "rt":rt, "rts":rts} )
+		user = res.get()
+		user.tweet_count = json_str
+		user.tweet_last = last
+		user.put()
+	else:
+		logging.error("gdb.save_count():")
+		logging.error("user_id: %d\ncount: %d, %d, %d, %d" % (user_id, sum, re, rt, rts))
+
+# Load user's count
+def load_count(user_id):
+	res = User.all().filter("user_id = ", user_id)
+	if res.count()==1:
+		user = res.get()
+		json_obj = json.loads( user.tweet_count )
+		return (json_obj['sum'], json_obj['re'], json_obj['rt'], json_obj['rts'], user.tweet_last)
+	else:
+		logging.error("gdb.load_count():")
+		logging.error("user_id: %d" % (user_id))
+	
+# Reset user's count
+def reset_count(user_id):
+	res = User.all().filter("user_id = ", user_id)
+	if res.count()==1:
+		user = res.get()
+		user.tweet_count = '{"rt": 0, "re": 0, "sum": 0, "rts": 0}'
+		user.put()
+	else:
+		logging.error("gdb.load_count():")
+		logging.error("user_id: %d" % (user_id))
--- main.py
+++ main.py
@@ -4,14 +4,13 @@
 
 import webapp2
 import logging
-
 import oauth, gdb
 
 CONSUMER_KEY = 'CjUSfZ1IDHrBe9dLu5Viyw'
 CONSUMER_SECRET = '3iUPswMl97gxxirCK5rVN3MvNqM5AcB1dTnxlmdMyQ'
-VERSION = 'dev'
 TWEET_MIN = 16
 AUTH_KEY = '0x0'
+VERSION = 'dev'
 
 class OauthS(webapp2.RequestHandler):
 	def get(self, mode=''):
@@ -23,10 +22,10 @@ class OauthS(webapp2.RequestHandler):
 				try:
 					url = client.get_authorization_url()
 					self.redirect(url)
-				except Exception,error_message:
-					logging.error("/oauth/auth Error:")
-					logging.error(error_message)
-					self.response.out.write( error_message )
+				except Exception, msg:
+					logging.error("/oauth/auth:")
+					logging.error(msg)
+					self.response.out.write( msg )
 				
 			else:
 				# Redirect unauth user to main page
@@ -44,16 +43,50 @@ class OauthS(webapp2.RequestHandler):
 				gdb.save_user(int(user_id), access_token, access_secret)
 				self.redirect( "%s/oauth/succ?user_id=%s" % (self.request.host_url, user_id) )
 				
-			except Exception,error_message:
-				logging.error("/oauth/verity Error:")
+			except Exception, msg:
+				logging.error("/oauth/verity:")
 				logging.error("auth_token: %s\nauth_verifier: %s" % (auth_token, auth_verifier))
 				logging.error("user_id: %s\ntoken: %s\nsecret: %s" % (user_id, access_token, access_secret))
-				logging.error( error_message )
-				self.response.out.write( error_message )
+				logging.error( msg )
+				self.response.out.write( msg )
 			
 		if mode=='succ':
 			self.response.out.write( 'You have authorize this app. <br/>$username: %s' % self.request.get("user_id") )
 		
+	
+class PostS(webapp2.RequestHandler):
+	def get(self):
+		# Ridirect to host if not call by cron.yaml
+		if not self.request.headers.has_key( "X-AppEngine-Cron" ) or not self.request.headers['X-AppEngine-Cron'] :
+			self.redirect(self.request.host_url)
+			return
+		
+		all_users = gdb.all_users()
+		client = oauth.TwitterClient( CONSUMER_KEY, CONSUMER_SECRET, "%s/oauth/verify" % self.request.host_url )
+		
+		for user in all_users:
+			# Load count and tweet it.
+			try:
+				(sum, re, rt, rts, last) = gdb.load_count(user.user_id)
+			except Exception, msg:
+				logging.error("/post:gdb")
+				logging.error("user_id: %d", user.user_id)
+				logging.error(msg)
+			try:
+				client.tweet(user.token, user.secret, (sum, re, rt, rts))
+			except Exception, msg:
+				logging.error("/post:tweet")
+				logging.error("user_id: %d", user.user_id)
+				logging.error(msg)
+			
+
+def format_date(ss, MONTH2NUMBER={'Jan':'01','Feb':'02','Mar':'03','Apr':'04','May':'05','Jun':'06', 'Jul':'07','Aug':'08','Sep':'09','Oct':'10','Nov':'11','Dec':'12'}):
+	return ''.join([ ss[26:30],MONTH2NUMBER[ss[4:7]],ss[8:10],ss[11:13],ss[14:16],ss[17:19] ])
+
+class UpdateS(webapp2.RequestHandler):
+	def get(self):
+		pass
+	
 
 class DefaultS(webapp2.RequestHandler):
 	def get(self):
@@ -71,5 +104,7 @@ class DefaultS(webapp2.RequestHandler):
 
 app = webapp2.WSGIApplication([
 		(r'/oauth/(.*)',OauthS	),
+		(r'/post',		PostS	),
+		(r'/update',	UpdateS	),
 		(r'/.*',		DefaultS)
 		])
